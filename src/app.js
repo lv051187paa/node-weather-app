8ccfa98a7c610dd2def59f58a20d9d8410507371
Andrii Pavliuk <v051187paa@users.noreply.github.com>
2019-08-25 01:49:01 +0300

    test ssh

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const publicDirectory = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialPath = path.join(__dirname, '../templates/partials');
 
-//Setup handlebars engine and views directory
+//Setup handlebars engine and views  directory
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialPath);
