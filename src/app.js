e855de1db4d943e11349a056d6a69571cfe7af19
Andrii Pavliuk <v051187paa@users.noreply.github.com>
2019-08-25 02:01:06 +0300

    prepare for deploy

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || '3000';
 
 //Define paths
 const publicDirectory = path.join(__dirname, '../public');
@@ -94,4 +95,4 @@ app.get('*', (req, res) => {
 	});
 });
 
-app.listen('3000', () => console.log('Listnening to port 3000...'));
+app.listen(port, () => console.log(`Listnening to port ${port}...`));
